03d84e253ef19b776b1c996276fbd07e0f023366
RubenvdW <ruben.vanderwolf@gmail.com>
2022-06-13 15:54:56 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Do not modify these lines
+__winc_id__ = '499e67d5cb54448e93cee7465be2c866'
+__human_name__ = 'calculate'
+
+# Add your code after this line
+broccoli = 2
+leek = 2
+potato = 3
+brussel_sprout = 7
+
+sum_one_each = broccoli + leek + potato + brussel_sprout
+avg_price = sum_one_each / 4
+
+num_potatoes = 7
+num_broccolis = 5
+num_leeks = 2
+num_brussel_sprouts = 10
+
+sum_total = (num_potatoes * potato) + (num_broccolis * broccoli) + \
+    (num_brussel_sprouts * brussel_sprout) + (num_leeks * leek)
+
+discount_percentage = 30
+
+discounted_sum_total = (sum_total / 100) * (100 - discount_percentage)
+
+print(discounted_sum_total)
